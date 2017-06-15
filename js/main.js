ee237158e0716d4ee8f211c3f72cb0ec35ff21a3
magentaFire <dafne.magenta@gmail.com>
2017-06-15 11:42:57 -0500

    Terminada lectura de temas en el foro

---
+++ js/main.js
@@ -0,0 +1,35 @@
+var api = {
+  url: "http://examen-laboratoria-sprint-5.herokuapp.com/topics/"
+}
+
+var plantillaTopics = '<div class="row">' +
+                        '<div class="col s12 m10">' +
+                          '<div class="card blue-grey darken-1">' +
+                            '<div class="card-content white-text">' +
+                              '<span class="">**titulo**  -  **autor**</span>' +
+                              '<span class="right">respuestas: **respuestas**</span>' +
+                            '</div>' +
+                          '</div>' +
+                        '</div>' +
+                      '</div>'
+
+$(document).ready(start);
+
+function start(){
+  $.getJSON(api.url, function(topics) {
+    topics.forEach(crearTopics)
+  });
+  $(".btn-crear-tema").click(crearNuevoTema)
+}
+
+function crearTopics(topic){
+  var $container = $(".themes-container")
+  var plantillaTopicsNueva = plantillaTopics.replace('**titulo**', topic.content)
+    .replace('**autor**', 'por: ' + topic.author_name)
+    .replace('**respuestas**', topic.responses_count);
+  $container.append(plantillaTopicsNueva);
+}
+
+function crearNuevoTema(){
+  
+}
